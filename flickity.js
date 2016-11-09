6174ac36c65056c932d0bed92b64d5e9a6adf62e
arnab <arnabnitb@gmail.com>
2016-11-09 18:52:22 +0530

    cleanup

--- flickity.js
+++ flickity.js
@@ -10,151 +10,6 @@
  */
 
 /**
- * Bridget makes jQuery widgets
- * v2.0.1
- * MIT license
- */
-
-/* jshint browser: true, strict: true, undef: true, unused: true */
-
-(function(window, factory) {
-    // universal module definition
-    /*jshint strict: false */
-    /* globals define, module, require */
-    if (typeof define == 'function' && define.amd) {
-        // AMD
-        define('jquery-bridget/jquery-bridget', ['jquery'], function(jQuery) {
-            return factory(window, jQuery);
-        });
-    } else if (typeof module == 'object' && module.exports) {
-        // CommonJS
-        module.exports = factory(
-            window,
-            require('jquery')
-        );
-    } else {
-        // browser global
-        window.jQueryBridget = factory(
-            window,
-            window.jQuery
-        );
-    }
-
-}(window, function factory(window, jQuery) {
-    'use strict';
-
-    // ----- utils ----- //
-
-    var arraySlice = Array.prototype.slice;
-
-    // helper function for logging errors
-    // $.error breaks jQuery chaining
-    var console = window.console;
-    var logError = typeof console == 'undefined' ? function() {} :
-        function(message) {
-            console.error(message);
-        };
-
-    // ----- jQueryBridget ----- //
-
-    function jQueryBridget(namespace, PluginClass, $) {
-        $ = $ || jQuery || window.jQuery;
-        if (!$) {
-            return;
-        }
-
-        // add option method -> $().plugin('option', {...})
-        if (!PluginClass.prototype.option) {
-            // option setter
-            PluginClass.prototype.option = function(opts) {
-                // bail out if not an object
-                if (!$.isPlainObject(opts)) {
-                    return;
-                }
-                this.options = $.extend(true, this.options, opts);
-            };
-        }
-
-        // make jQuery plugin
-        $.fn[namespace] = function(arg0 /*, arg1 */ ) {
-            if (typeof arg0 == 'string') {
-                // method call $().plugin( 'methodName', { options } )
-                // shift arguments by 1
-                var args = arraySlice.call(arguments, 1);
-                return methodCall(this, arg0, args);
-            }
-            // just $().plugin({ options })
-            plainCall(this, arg0);
-            return this;
-        };
-
-        // $().plugin('methodName')
-        function methodCall($elems, methodName, args) {
-            var returnValue;
-            var pluginMethodStr = '$().' + namespace + '("' + methodName + '")';
-
-            $elems.each(function(i, elem) {
-                // get instance
-                var instance = $.data(elem, namespace);
-                if (!instance) {
-                    logError(namespace + ' not initialized. Cannot call methods, i.e. ' +
-                        pluginMethodStr);
-                    return;
-                }
-
-                var method = instance[methodName];
-                if (!method || methodName.charAt(0) == '_') {
-                    logError(pluginMethodStr + ' is not a valid method');
-                    return;
-                }
-
-                // apply method, get return value
-                var value = method.apply(instance, args);
-                // set return value if value is returned, use only first value
-                returnValue = returnValue === undefined ? value : returnValue;
-            });
-
-            return returnValue !== undefined ? returnValue : $elems;
-        }
-
-        function plainCall($elems, options) {
-            $elems.each(function(i, elem) {
-                var instance = $.data(elem, namespace);
-                if (instance) {
-                    // set options & init
-                    instance.option(options);
-                    instance._init();
-                } else {
-                    // initialize new instance
-                    instance = new PluginClass(elem, options);
-                    $.data(elem, namespace, instance);
-                }
-            });
-        }
-
-        updateJQuery($);
-
-    }
-
-    // ----- updateJQuery ----- //
-
-    // set $.bridget for v1 backwards compatibility
-    function updateJQuery($) {
-        if (!$ || ($ && $.bridget)) {
-            return;
-        }
-        $.bridget = jQueryBridget;
-    }
-
-    updateJQuery(jQuery || window.jQuery);
-
-    // -----  ----- //
-
-    return jQueryBridget;
-
-}));
-
-/**
  * EvEmitter v1.0.3
  * Lil' event emitter
  * MIT License
@@ -482,23 +337,22 @@
  */
 
 /*jshint browser: true, strict: true, undef: true, unused: true */
+( function( window, factory ) {
+  /*global define: false, module: false */
+  'use strict';
+  // universal module definition
+  if ( typeof define == 'function' && define.amd ) {
+    // AMD
+    define( 'desandro-matches-selector/matches-selector',factory );
+  } else if ( typeof module == 'object' && module.exports ) {
+    // CommonJS
+    module.exports = factory();
+  } else {
+    // browser global
+    window.matchesSelector = factory();
+  }
 
-(function(window, factory) {
-    /*global define: false, module: false */
-    'use strict';
-    // universal module definition
-    if (typeof define == 'function' && define.amd) {
-        // AMD
-        define('desandro-matches-selector/matches-selector', factory);
-    } else if (typeof module == 'object' && module.exports) {
-        // CommonJS
-        module.exports = factory();
-    } else {
-        // browser global
-        window.matchesSelector = factory();
-    }
-
-}(window, function factory() {
+}( window, function factory() {
     'use strict';
 
     var matchesMethod = (function() {
@@ -768,6 +622,8 @@
 
 }));
 
+
+/* --------------------------------------------- Flickity logic starts --------------------------------------- */
 // Flickity.Cell
 (function(window, factory) {
     // universal module definition
@@ -1997,15 +1853,16 @@
 
     utils.htmlInit(Flickity, 'flickity');
 
-    if (jQuery && jQuery.bridget) {
-        jQuery.bridget('flickity', Flickity);
-    }
-
     Flickity.Cell = Cell;
 
     return Flickity;
 
 }));
+
+
+/* --------------------------------------------- Flickity logic ends --------------------------------------- */
+
+
 
 /*!
  * Unipointer v2.1.0
@@ -2890,8 +2747,10 @@
         var condition = this.options.contain && !this.options.wrapAround ?
             // if contain, keep going if distance is equal to minDistance
             function(d, md) {
-                return d <= md; } : function(d, md) {
-                return d < md; };
+                return d <= md;
+            } : function(d, md) {
+                return d < md;
+            };
         while (condition(distance, minDistance)) {
             // measure distance to next cell
             index += increment;
@@ -3345,22 +3204,14 @@
         define('flickity/js/index', [
             './flickity',
             './drag',
-            './prev-next-button',
-            './page-dots',
-            './player',
-            './add-remove-cell',
-            './lazyload'
+            './prev-next-button'
         ], factory);
     } else if (typeof module == 'object' && module.exports) {
         // CommonJS
         module.exports = factory(
             require('./flickity'),
             require('./drag'),
-            require('./prev-next-button'),
-            require('./page-dots'),
-            require('./player'),
-            require('./add-remove-cell'),
-            require('./lazyload')
+            require('./prev-next-button')
         );
     }
 
