ed3c85e043daa8c8526b89279aa4f87314fa1142
nikhilparmar86 <nikhilparmar86@yahoo.co.in>
2016-11-11 16:20:21 +0530

    GetSize

--- tabs.js
+++ tabs.js
@@ -1,7 +1,23 @@
 var TabSwipeLayoutGroupModule = (function(p) {
 
     var LOGGING = false,
-    _events = {};
+    _events = {},
+    dimensions = 
+        ["paddingLeft", 
+        "paddingRight", 
+        "paddingTop", 
+        "paddingBottom", 
+        "marginLeft", 
+        "marginRight", 
+        "marginTop", 
+        "marginBottom", 
+        "borderLeftWidth", 
+        "borderRightWidth", 
+        "borderTopWidth", 
+        "borderBottomWidth"],
+    dimensionsLen = dimensions.length,
+    isBorderCheck = false,
+    isBoxSizeOuter = false,
 
     _isValidListener = function (listener) {
         if (typeof listener === 'function' || listener instanceof RegExp) {
@@ -138,6 +154,102 @@ var TabSwipeLayoutGroupModule = (function(p) {
             }
         }
         return -1;
+    },
+
+    _getStyleSize = function(value) {
+        var num = parseFloat(value);
+        // not a percent like '100%', and a number
+        var isValid = value.indexOf("%") == -1 && !isNaN(num);
+        return isValid && num;
+    },
+
+    _checkForBorder = function(){
+        // setup once
+        if (isBorderCheck) {
+            return;
+        }
+        isBorderCheck = true;
+        // -------------------------- box sizing -------------------------- //
+        /**
+         * WebKit measures the outer-width on style.width on border-box elems
+         * IE & Firefox<29 measures the inner-width
+         */
+        var div = document.createElement("div");
+        div.style.width = "400px";
+        div.style.padding = "1px 2px 3px 4px";
+        div.style.borderStyle = "solid";
+        div.style.borderWidth = "1px 2px 3px 4px";
+        div.style.boxSizing = "border-box";
+        var body = document.body || document.documentElement;
+        body.appendChild(div);
+        var style = window.getComputedStyle(div);
+        isBoxSizeOuter = _getStyleSize(style.width) == 400;
+        body.removeChild(div);
+    },
+
+     _getZeroSize = function() {        
+        var sizeObj = {
+            width: 0,
+            height: 0,
+            innerWidth: 0,
+            innerHeight: 0,
+            outerWidth: 0,
+            outerHeight: 0
+        };
+        for (var i = 0; i < dimensionsLen; i++) {
+            var dimension = dimensions[i];
+            sizeObj[dimension] = 0;
+        }
+        return sizeObj;
+    },
+
+    _getSize =  function(elem) {
+        _checkForBorder();
+        if (typeof elem == "string") {
+            elem = document.querySelector(elem);
+        }
+        if (!elem || typeof elem != "object" || !elem.nodeType) {
+            return;
+        }
+        var style = window.getComputedStyle(elem);
+        
+        if (style.display == "none") {
+            return _getZeroSize();
+        }
+        var sizeObj = {};
+        sizeObj.width = elem.offsetWidth;
+        sizeObj.height = elem.offsetHeight;
+        var isBorderBox = sizeObj.isBorderBox = style.boxSizing == "border-box";
+        for (var i = 0; i < dimensionsLen; i++) {
+            var dimension = dimensions[i];
+            var value = style[dimension];
+            var num = parseFloat(value);
+            // any 'auto', 'medium' value will be 0
+            sizeObj[dimension] = !isNaN(num) ? num : 0;
+        }
+        var paddingWidth = sizeObj.paddingLeft + sizeObj.paddingRight;
+        var paddingHeight = sizeObj.paddingTop + sizeObj.paddingBottom;
+        var marginWidth = sizeObj.marginLeft + sizeObj.marginRight;
+        var marginHeight = sizeObj.marginTop + sizeObj.marginBottom;
+        var borderWidth = sizeObj.borderLeftWidth + sizeObj.borderRightWidth;
+        var borderHeight = sizeObj.borderTopWidth + sizeObj.borderBottomWidth;
+        var isBorderBoxSizeOuter = isBorderBox && isBoxSizeOuter;
+        // overwrite width and height if we can get it from style
+        var styleWidth = getStyleSize(style.width);
+        if (styleWidth !== false) {
+            sizeObj.width = styleWidth + ( // add padding and border unless it's already including it
+                isBorderBoxSizeOuter ? 0 : paddingWidth + borderWidth);
+        }
+        var styleHeight = getStyleSize(style.height);
+        if (styleHeight !== false) {
+            sizeObj.height = styleHeight + ( // add padding and border unless it's already including it
+                isBorderBoxSizeOuter ? 0 : paddingHeight + borderHeight);
+        }
+        sizeObj.innerWidth = sizeObj.width - (paddingWidth + borderWidth);
+        sizeObj.innerHeight = sizeObj.height - (paddingHeight + borderHeight);
+        sizeObj.outerWidth = sizeObj.width + marginWidth;
+        sizeObj.outerHeight = sizeObj.height + marginHeight;
+        return sizeObj;
     }
 
 
